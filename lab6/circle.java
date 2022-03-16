cc42eae2fcb697761537437afed510b89660d1e2
grrissa <marissanesteban@gmail.com>
2022-03-16 15:13:00 -0700

    fshapes

--- lab6/circle.java
+++ lab6/circle.java
@@ -1,5 +1,40 @@
 package lab6;
+import java.util.Scanner;
+import java.lang.Math;
 
-public class circle {
-    
+public class circle extends Shape {
+    private double radius = 0;
+
+    public Cicle() {
+        super("Rectangle");
+    }
+
+    public circle(double r) {
+        super("Circle");
+        radius = r;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double computeArea() {
+        return Math.PI * (radius**2);
+    }
+
+    public double computePerimeter() {
+        return 2*Math.PI*radius;
+    }
+
+    public void readShapeData() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the width of the Rectangle");
+        width = in.nextDouble();
+        System.out.println("Enter the height of the Rectangle");
+        height = in.nextDouble();        
+    }
+
+    public String toString() {
+        return super.toString() + ": width is" + width + ", height is " + height;
+    }
 }
