b439e59acf007cf8a86f1803b1c34c5d4de9a86c
Alizea Hinz <ahinz@sandiego.edu>
2022-03-16 15:30:20 -0700

    change

--- proj2/proj2.java
+++ proj2/proj2.java
@@ -1,14 +1,16 @@
+package proj2;
 /*************************************************************************************
-Brief Summary: Programthat reads an ASCII file that contains a set of numbers of type int 
-and outputs all the numbers to another file, but without any duplicated numbers
+Brief Summary: 
 Authors: Marissa Esteban and Alizea Hinz
-Last Date Modified: 2/27/2022
+Last Date Modified: 
 *************************************************************************************/
+
+//ASK ABOUT AVERAGE AND WHETHER IT IS ALL NUMBER IN THE FILE OR JUST THE ONES WE ARE PRINTING
 
 import java.util.Scanner;
 import java.io.*; 
 
-public class proj2_2 {
+public class proj2 {
     static char repeat_again = 'n';
     public static void main (String[] args){
         
@@ -17,51 +19,40 @@ public class proj2_2 {
             String inputLine;
 
             try {
-                // asks user to input file to read
                 Scanner in = new Scanner(System.in);
                 System.out.println("\nPlease enter the name of an ASCII file that contains a sorted list of integer numbers, one per line:");
                 String input_file = in.nextLine();
                 input = new BufferedReader(new FileReader(input_file));
 
-                // creates a new file for the program to output
                 Scanner out = new Scanner(System.in);
                 System.out.println("\nPlease enter the name of the output file that saves the result from the program:");
                 String output_file = out.nextLine();
                 PrintWriter output = new PrintWriter(new FileOutputStream(output_file));
 
-                // loop that reads the infile line by line and prints it to the output file 
-                double sum = 0.0;
+                int sum = 0;
                 int count = 0;
                 int past_val = 0;
                 while ((inputLine = input.readLine()) != null) {
                     int num = Integer.parseInt(inputLine);
-
-                    //first time through the loop, initialize past_val
-                    if (count == 0) { 
+                    sum += num;
+                    count +=1;
+                    if (count == 1) { //first time through the loop, initialize past_val
                         past_val = num;
                         output.println(num);
-                        sum += num;
-                        count +=1;
                     }
-                    // only print if the next number is bigger, and update past_val
-                    else if (past_val < num) { 
+                    else if (past_val < num) { // only print if the next number is bigger, and update past_val
                         output.println(num);
                         past_val = num;
-                        sum += num;
-                        count +=1;
                     }
                 }
                 if (count != 0) {
                     double average = sum/count;
-                    String rounded_average = String.format("%.2f", average);
-                    output.println(rounded_average);
+                    output.println(average);
                 }
                 input.close();
                 output.close();
 
             } // end try
-
-            // catching exceptions
             catch (FileNotFoundException e) {
                 System.out.println(e.getMessage());
                 System.exit(1); 
@@ -70,10 +61,6 @@ public class proj2_2 {
                 System.out.println(e.getMessage());
                 System.exit(1); //IO error, exit program
             } // end catch
-            catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                System.exit(1);
-            }
 
             repeat_validation();
         
--- proj2/proj2_2.java
+++ proj2/proj2_2.java
@@ -1,15 +1,15 @@
+package proj2;
 /*************************************************************************************
-Brief Summary: 
+Brief Summary: Programthat reads an ASCII file that contains a set of numbers of type int 
+and outputs all the numbers to another file, but without any duplicated numbers
 Authors: Marissa Esteban and Alizea Hinz
-Last Date Modified: 
+Last Date Modified: 2/27/2022
 *************************************************************************************/
-
-//ASK ABOUT AVERAGE AND WHETHER IT IS ALL NUMBER IN THE FILE OR JUST THE ONES WE ARE PRINTING
 
 import java.util.Scanner;
 import java.io.*; 
 
-public class proj2 {
+public class proj2_2 {
     static char repeat_again = 'n';
     public static void main (String[] args){
         
@@ -18,40 +18,51 @@ public class proj2 {
             String inputLine;
 
             try {
+                // asks user to input file to read
                 Scanner in = new Scanner(System.in);
                 System.out.println("\nPlease enter the name of an ASCII file that contains a sorted list of integer numbers, one per line:");
                 String input_file = in.nextLine();
                 input = new BufferedReader(new FileReader(input_file));
 
+                // creates a new file for the program to output
                 Scanner out = new Scanner(System.in);
                 System.out.println("\nPlease enter the name of the output file that saves the result from the program:");
                 String output_file = out.nextLine();
                 PrintWriter output = new PrintWriter(new FileOutputStream(output_file));
 
-                int sum = 0;
+                // loop that reads the infile line by line and prints it to the output file 
+                double sum = 0.0;
                 int count = 0;
                 int past_val = 0;
                 while ((inputLine = input.readLine()) != null) {
                     int num = Integer.parseInt(inputLine);
-                    sum += num;
-                    count +=1;
-                    if (count == 1) { //first time through the loop, initialize past_val
+
+                    //first time through the loop, initialize past_val
+                    if (count == 0) { 
                         past_val = num;
                         output.println(num);
+                        sum += num;
+                        count +=1;
                     }
-                    else if (past_val < num) { // only print if the next number is bigger, and update past_val
+                    // only print if the next number is bigger, and update past_val
+                    else if (past_val < num) { 
                         output.println(num);
                         past_val = num;
+                        sum += num;
+                        count +=1;
                     }
                 }
                 if (count != 0) {
                     double average = sum/count;
-                    output.println(average);
+                    String rounded_average = String.format("%.2f", average);
+                    output.println(rounded_average);
                 }
                 input.close();
                 output.close();
 
             } // end try
+
+            // catching exceptions
             catch (FileNotFoundException e) {
                 System.out.println(e.getMessage());
                 System.exit(1); 
@@ -60,6 +71,10 @@ public class proj2 {
                 System.out.println(e.getMessage());
                 System.exit(1); //IO error, exit program
             } // end catch
+            catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.exit(1);
+            }
 
             repeat_validation();
         
