b6cb7e91065b3038d1bcd1cf8180201b7a2d9fc7
grrissa <marissanesteban@gmail.com>
2022-03-17 21:20:10 -0700

    lakjsdflka

--- lab6/ComputeAreaAndPerimeter.java
+++ lab6/ComputeAreaAndPerimeter.java
@@ -1,7 +1,7 @@
 /*****************************************************************************************
 Brief Summary: Program that prompts user for shape and computes the area and perimeter.
 Authors: Marissa Esteban and Alizea Hinz
-Last Date Modified: 2/23/2022
+Last Date Modified: 3/17/2022
 *****************************************************************************************/
 package lab6;
 import java.util.Scanner;
