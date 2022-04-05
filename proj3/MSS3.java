451053e8208b6c40b3a67d76df0e2141d1bf9ae8
Alizea Hinz <ahinz@sandiego.edu>
2022-04-05 12:01:09 -0700

    works

--- proj3/MSS3.java
+++ proj3/MSS3.java
@@ -16,18 +16,15 @@ public class MSS3 {
         int result = sum(nums_ints, 0, nums_ints.length-1);
         System.out.println("Maximum Sum = " + result);
     }
-    public static int sum(int[] num, int low, int high){
-        if (high == low)
-            return num[high];
+    
+    public static int sum(int[] nums, int low, int high){
+        if (high == low) //Base Case
+            return nums[high];
 
-        else {
+        else{
             int mid = (high + low)/ 2;
-            //int left_side_max = sum(num, low, mid);
-           // int right_side_max = sum(num, mid+1, high);
-            //int mid_sum_max = mid_sum(num, low, mid, high);
-           // return Math.max(Math.max(left_side_max, right_side_max), mid_sum_max);
 
-           return Math.max(Math.max(sum(num, low, mid), sum(num, mid+1, high)), mid_sum(num, low, mid, high));
+            return Math.max(Math.max(sum(nums, low, mid), sum(nums, mid+1, high)), mid_sum(nums, low, mid, high));
         }
     }
 
@@ -36,28 +33,19 @@ public class MSS3 {
         int maxSumLeft = 0, sum = 0;
         for(int i = mid; i>=low; i--){
             sum += num[i];
-            if (sum>maxSumLeft){
+            if (sum>maxSumLeft)
                 maxSumLeft= sum;
-            }
-            else {
-                if (sum<0) sum = 0;
-            }
         }
 
         int maxSumRight = 0;
         sum = 0;
         for(int i = mid+1; i<=high; i++){
             sum += num[i];
-            if (sum>maxSumRight){
+            if (sum>maxSumRight)
                 maxSumRight = sum;
-            }
-            else {
-                if (sum<0) sum = 0;
-            }
         }
         return Math.max(Math.max(maxSumLeft + maxSumRight,maxSumLeft),maxSumRight);
     }
-
 
     public static int[] convert_array(String nums_str) {
         String[] nums_array = nums_str.split(",");
