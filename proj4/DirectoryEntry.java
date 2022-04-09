d99ca3defb47d11b418b755624efba39239562ed
grrissa <marissanesteban@gmail.com>
2022-04-09 15:45:36 -0700

    asdfa

---
+++ proj4/DirectoryEntry.java
@@ -0,0 +1,8 @@
+package proj4;
+
+public class DirectoryEntry {
+    public static void main(String[] args) {
+        String name;
+        String number;
+    }
+}
---
+++ proj4/PhoneDirectory.java
@@ -0,0 +1,19 @@
+package proj4;
+import java.util.*;
+
+public class PhoneDirectory {
+    public static void main(String[] args) {
+        private List<DirectoryEntry> theDirectory = new ArrayList<>();
+    }
+
+    public PhoneDirectory() {
+
+    }
+
+    
+    
+    
+
+}
+
+
