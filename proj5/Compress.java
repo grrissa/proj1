9f7cd6ebd895ebfe90b682ec97c8c3bf82b5acb9
Alizea Hinz <ahinz@sandiego.edu>
2022-04-22 10:56:16 -0700

    beginning

---
+++ proj5/Compress.java
@@ -0,0 +1,5 @@
+public class Compress {
+    public static void main(String[] args) { //args will be the name of the file
+        //base table size based off of file size... want to avoid rehashing
+    }
+}
