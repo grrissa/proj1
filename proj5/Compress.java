113016a19e2e53b99347abc679fab7c1c3e3c80f
Alizea Hinz <ahinz@sandiego.edu>
2022-04-22 10:57:19 -0700

    pacakge

--- proj5/Compress.java
+++ proj5/Compress.java
@@ -1,3 +1,5 @@
+package proj5;
+
 public class Compress {
     public static void main(String[] args) { //args will be the name of the file
         //base table size based off of file size... want to avoid rehashing
--- proj5/Decompress.java
+++ proj5/Decompress.java
@@ -1,3 +1,5 @@
+package proj5;
+
 public class Decompress {
     public static void main(String[] args) {
         
