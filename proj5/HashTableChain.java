967b34a45f5b44c0a431fc7e2549d1561129aa67
grrissa <marissanesteban@gmail.com>
2022-04-29 16:16:16 -0700

    rehash

--- proj5/HashTableChain.java
+++ proj5/HashTableChain.java
@@ -37,8 +37,8 @@ public class HashTableChain<k,v> implements KWHashMap<k,v>{
 
     private LinkedList<Entry<k,v>>[] table; // array of linked lists
     private int numKeys;
-    private static final int CAPACITY; //tbd
-    private static final double LOAD_THRESHOLD; //tbd
+    private static int CAPACITY; // i am making this a non final bc in rehash we have to change it again
+    private static final double LOAD_THRESHOLD = 0.75; //tbd? i found num this on the internet
     //may need more
 
     public HashTableChain() {// CONSTRUCTOR
@@ -51,7 +51,18 @@ public class HashTableChain<k,v> implements KWHashMap<k,v>{
         numKeys = 0;
     }
 
+    public boolean isEmpty(){
+        return numKeys==0;
+    }
+
+    public int size(){
+        return numKeys;
+    }
+
     public v get(Object key){
+        /*
+        Uses the object key to search and return for the value that is associated with the key
+        */
         int index = key.hashCode() % table.length;
         if (index < 0){
             index += table.length;
@@ -67,15 +78,12 @@ public class HashTableChain<k,v> implements KWHashMap<k,v>{
         return null;
     }
 
-    public boolean isEmpty(){
-        return numKeys==0;
-    }
-
-    public int size(){
-        return numKeys;
-    }
-
     public v put(k key, v value){
+        /*
+        (key, val) pair is inserted into the table and numKeys incremented. If key
+        is already in table, value changed to value in param.
+        */
+
         int index = key.hashCode() % table.length;
         if (index < 0){
             index += table.length;
@@ -91,11 +99,17 @@ public class HashTableChain<k,v> implements KWHashMap<k,v>{
             }
         }
 
-        if (numKeys > (LOAD_THRESHOLD * table.length)) rehash();
+        table[index].addFirst(new Entry<>(key, value));
+        numKeys++;
+        if (numKeys > (LOAD_THRESHOLD * table.length)) 
+            rehash();
         return null;
     }
 
     public v remove(Object key){
+        /*
+        Will remove (key, val) entry pair from the table
+        */
         int index = key.hashCode() % table.length;
         if (index < 0){
             index += table.length;
@@ -103,21 +117,79 @@ public class HashTableChain<k,v> implements KWHashMap<k,v>{
         if (table[index] == null){
            return null;
         }
+
+        int counter = 0; // to specify what item in linked list to remove
         for(Entry<k,v> nextItem:table[index]){
             if (nextItem.getKey().equals(key)){
-                table[index].remove(); //FIXME
+                table[index].remove(counter); 
                 numKeys--; //decrement 
+
                 if(table[index].isEmpty()){
                     table[index] = null;
                 }
                 return nextItem.getValue();
             }
+            counter++;
         }
 
         return null; // if key is not in the table
     }
 
+    public static boolean isPrime(int n){
+
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+         
+        if (n % 2 == 0 || n % 3 == 0) return false;
+         
+        for (int i = 5; i * i <= n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+         
+        return true;
+    }
+     
+    public static int nextPrime(int N) {
+    
+        if (N <= 1)
+            return 2;
+     
+        int prime = N;
+        boolean found = false;
+
+        while (!found){
+            prime++;
+     
+            if (isPrime(prime))
+                found = true;
+        }
+     
+        return prime;
+    }
+     
     public void rehash() {
+
+        int next_prime = (CAPACITY*2);
+        LinkedList<Entry<k,v>>[] new_table = new LinkedList[next_prime];
+
+        //will go through every index in table and then every entry in linked list to add entry into new table
+        for (int i = 0; i < table.length; i++){
+            for(Entry<k,v> nextItem:table[i]){
+
+                int index = (nextItem.getKey().hashCode()) % new_table.length;
+                if (index < 0){
+                    index += new_table.length;
+                }
+                if (new_table[index] == null){
+                    new_table[index] = new LinkedList<>();
+                }
+                table[index].addFirst(new Entry<>(nextItem.getKey(), nextItem.getValue()));
+            }
+        }
+
+        table = new_table;
+        CAPACITY = next_prime;
+
 
     }
         
